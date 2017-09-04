8e06e508683537bf13e29bee05d048db2847605e
Tiago Resende <tiagor87@gmail.com>
2017-09-04 14:39:53 -0300

    simple test from @angular/material

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
